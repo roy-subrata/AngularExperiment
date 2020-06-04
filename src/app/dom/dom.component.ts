507372f10e1aa17f9eaf080af5df61538e8f4510
roy-subrata <roy.subrat.cs@gmail.com>
2020-06-05 00:47:35 +0600

    add text

--- src/app/dom/dom.component.ts
+++ src/app/dom/dom.component.ts
@@ -4,7 +4,7 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: "r-dom",
-    template: "<p *ngIf='true'>I am from dom</p>"
+    template: "<p *ngIf='true'>I am from dom.how are you?</p>"
 })
 export class DomComponent {
 
